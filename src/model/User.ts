8aa0765312076ed312e61278348825ee5e1cb69b
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2024-04-13 08:49:28 +0530

    Schema's defined

--- src/model/User.ts
+++ src/model/User.ts
@@ -21,3 +21,50 @@ const MessageSchema: Schema<Message> = new Schema({
     default: Date.now,
   },
 });
+
+// user schema
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  messages: Message[];
+}
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+\@.+\..+/, "please use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "verify code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "verify code expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
